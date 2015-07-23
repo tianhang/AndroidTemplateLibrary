c4bab726cd84e523bc0aa403e503b265743c103e
william <476855396@qq.com>
2015-07-23 10:42:19 +0800

    refreshListView

--- pullrefresh/src/main/java/com/example/student/pullrefresh/RefreshListView.java
+++ pullrefresh/src/main/java/com/example/student/pullrefresh/RefreshListView.java
@@ -1,38 +1,22 @@
 package com.example.student.pullrefresh;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
-public class RefreshListView extends ActionBarActivity {
+public class RefreshListView extends ListView  {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_refresh_list_view);
+
+    public RefreshListView(Context context) {
+        super(context);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_refresh_list_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public RefreshListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 }
