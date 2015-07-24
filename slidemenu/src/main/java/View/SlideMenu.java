f16e60f018cb6d2e34dc45f90c9b0a2a6e778843
william <476855396@qq.com>
2015-07-24 13:33:03 +0800

    update

--- slidemenu/src/main/java/View/SlideMenu.java
+++ slidemenu/src/main/java/View/SlideMenu.java
@@ -1,7 +1,24 @@
 package View;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
 /**
  * Created by student on 23/7/15.
  */
-public class SlideMenu {
+public class SlideMenu extends ViewGroup {
+
+    public SlideMenu(Context context) {
+        super(context);
+    }
+
+    public SlideMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
 }
